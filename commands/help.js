b963a1573ff33383035aed9e49a9e604cebfe9a5
notmythdora <59923012+fakemythdora@users.noreply.github.com>
2022-07-01 21:52:04 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
         let embed = new MessageEmbed()
             .setColor('#FFFFFF')	
             .setTitle('Command List')
-            .setDescription("`suggest`: Tạo ra một suggest\n`feedback`: Tạo ra một feedback")
+            .setDescription("`suggest`: Tạo ra một suggest\n`feedback`: Tạo ra một feedback\n`membercount`: Hiển thị số lượng thành viên trên server này\n`roleinfo`: Kiểm tra thông tin của role\n`serverinfo`: Kiểm tra thông tin của server\n`userinfo`: Kiểm tra thông tin account của bạn hoặc account của ai đó khác")
             .setTimestamp()
         
             return message.channel.send({ embeds: [ embed ] })
